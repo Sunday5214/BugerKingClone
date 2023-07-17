a7f245ebf8829c0f1ee7a595e8adf5130a032512
tokim <kimtaeo5214@gmail.com>
2023-07-17 09:14:42 +0900

    add. 백버튼 처리

--- MugerKing/MainPage.xaml.cs
+++ MugerKing/MainPage.xaml.cs
@@ -1,10 +1,38 @@
-﻿namespace MugerKing;
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+namespace MugerKing;
 
 public partial class MainPage : ContentPage
 {
+    bool canCloseApp = false;
+
 	public MainPage()
 	{
 		InitializeComponent();
 	}
+
+    private async void ShowBackButtonToast()
+    {
+        string text = "한 번 더 누르면 종료됩니다.";
+        ToastDuration duration = ToastDuration.Short;
+        double fontSize = 14;
+
+        var toast = Toast.Make(text, duration, fontSize);
+
+        await toast.Show();
+    }
+
+    protected override bool OnBackButtonPressed()
+    {
+        if(!canCloseApp)
+        {
+            ShowBackButtonToast();
+            canCloseApp = true;
+            return true;
+        }
+        else
+            return false;
+    }
 }
 
