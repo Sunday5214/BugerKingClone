ccc608e5f620346976d8f96f748c91a7b83f2256
김태오(Taeo Kim) <38588097+Sunday5214@users.noreply.github.com>
2023-08-13 15:58:24 +0900

    add. homePage
    commit 35162ac7fd7b3cf7703544c7a2ed23ab819a0121
    Author: 김태오(Taeo Kim) <38588097+Sunday5214@users.noreply.github.com>
    Date:   Sun Aug 13 15:48:23 2023 +0900
    
        add. home page 자잘한 작업
    
    commit c9c359d1d0f6f01c988c490e5c97dec337703587
    Author: 김태오(Taeo Kim) <38588097+Sunday5214@users.noreply.github.com>
    Date:   Sun Aug 13 13:09:35 2023 +0900
    
        add. home screen

---
+++ MugerKing/Models/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MugerKing.Models
+{
+    public class Event
+    {
+        public Event(string url, string imageUrl)
+        {
+            Url = url;
+            ImageUrl = imageUrl;    
+        }
+
+        public string Url { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ MugerKing/ViewModels/HomeViewModel.cs
@@ -0,0 +1,56 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MugerKing.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MugerKing.ViewModels
+{
+    public partial class HomeViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableCollection<Event> eventList;
+
+        [ObservableProperty]
+        private ObservableCollection<Buger> bugerList;
+
+        [ObservableProperty]
+        private int currentIdx;
+
+        [RelayCommand]
+        private void CurrentImagItemChanged(object currentItem)
+        {
+            if (currentItem is Event eventObject)
+            {
+                CurrentIdx = EventList.IndexOf(eventObject) + 1;
+            }
+        }
+
+        public HomeViewModel()
+        {
+            EventList = new ObservableCollection<Event>()
+            {
+                new Event("", "homeimage1.png"),
+                new Event("", "homeimage1.png"),
+                new Event("", "homeimage1.png"),
+                new Event("", "homeimage1.png"),
+            };
+
+            BugerList = new ObservableCollection<Buger>()
+            {
+                new Buger(0, "마우이 와퍼", "bugerkinglogo.svg", 2),
+                new Buger(1, "마우이 와퍼", "bugerkinglogo.svg", 2),
+                new Buger(2, "마우이 와퍼", "bugerkinglogo.svg", 2),
+                new Buger(3, "마우이 와퍼", "bugerkinglogo.svg", 2),
+                new Buger(4, "마우이 와퍼", "bugerkinglogo.svg", 2),
+                new Buger(5, "마우이 와퍼", "bugerkinglogo.svg", 2),
+                new Buger(6, "마우이 와퍼", "bugerkinglogo.svg", 2),
+                new Buger(7, "마우이 와퍼", "bugerkinglogo.svg", 2),
+            };
+        }
+    }
+}
